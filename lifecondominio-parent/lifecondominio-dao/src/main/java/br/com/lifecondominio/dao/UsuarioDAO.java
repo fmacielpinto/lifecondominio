47851f4ebdea059b22489974de00e7c4e56e844d
fmacielpinto <fmacielpinto@gmail.com>
2016-04-17 22:41:56 -0300

    Update UsuarioDAO.java

--- lifecondominio-parent/lifecondominio-dao/src/main/java/br/com/lifecondominio/dao/UsuarioDAO.java
+++ lifecondominio-parent/lifecondominio-dao/src/main/java/br/com/lifecondominio/dao/UsuarioDAO.java
@@ -10,18 +10,11 @@ public class UsuarioDAO extends BaseDAO<Usuario> {
 
 	public UsuarioDAO(Class<Usuario> entityClass) {
 		super(entityClass);
-		// TODO Auto-generated constructor stub
 	}
 
 	public void delete(Usuario usuario) {
-	
 	}
 
 	public void save(Usuario c) {
-		// TODO Auto-generated method stub
-		
 	}
-
-	
-
 }
